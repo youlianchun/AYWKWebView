ad07db7d76dd2b34630428967d753a4c6f6e294e
YLCHUN <985050622@qq.com>
2017-06-29 16:20:20 +0800

    no message

--- AYWKWebView/AYBionicWebView.m
+++ AYWKWebView/AYBionicWebView.m
@@ -30,7 +30,6 @@ void aywkw_replaceMethod(Class class, SEL originSelector, SEL newSelector);
 #pragma mark -
 #pragma mark - AYBionicWebView
 
-
 @interface AYBionicWebView ()
 @property (nonatomic, weak) UIViewController *viewController;
 @property (nonatomic, strong) NSMutableDictionary <NSString*, NSArray<UINavigationItem*>*> *navigationItemsDict;
@@ -51,7 +50,7 @@ void aywkw_replaceMethod(Class class, SEL originSelector, SEL newSelector);
 - (NSString *)md5 {
     const char *cStr = [self UTF8String];
     unsigned char digest[CC_MD5_DIGEST_LENGTH];
-    CC_MD5( cStr, strlen(cStr), digest ); // This is the md5 call
+    CC_MD5( cStr, (CC_LONG)strlen(cStr), digest ); // This is the md5 call
     NSMutableString *output = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
     for(int i = 0; i < CC_MD5_DIGEST_LENGTH; i++)
         [output appendFormat:@"%02x", digest[i]];
